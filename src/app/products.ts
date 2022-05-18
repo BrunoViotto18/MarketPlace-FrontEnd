5b0316e064b4d2ceec17943eaae89ccdafaf13d7
Nathan <nathan_cordeiro03@hotmail.com>
2022-05-18 16:18:21 -0300

    Alterações nathan

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
     name: string;
     price: number;
     description: string;
+    image: string;
   }
   
   export const products = [
@@ -10,49 +11,8 @@ export interface Product {
       id: 1,
       name: 'Phone XL',
       price: 799,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      id: 2,
-      name: 'Phone Mini',
-      price: 699,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      id: 3,
-      name: 'Phone Standard',
-      price: 299,
-      description: ''
-    },
-    {
-      id: 4,
-      name: 'Phone Max',
-      price: 700,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      id: 5,
-      name: 'Phone Small',
-      price: 350,
-      description: 'A great phone with one of the best cameras'
-    },
-    {
-      id: 5,
-      name: 'Phone Regular',
-      price: 600,
-      description: ''
-    },
-    {
-      id: 6,
-      name: 'Phone Max',
-      price: 700,
-      description: 'A large phone with one of the best screens'
-    },
-    {
-      id: 7,
-      name: 'Phone Small',
-      price: 350,
-      description: 'A great phone with one of the best cameras'
+      description: 'A large phone with one of the best screens',
+      image: "a"
     }
   ];
   
