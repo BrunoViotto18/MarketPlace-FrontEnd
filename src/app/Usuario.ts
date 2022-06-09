774e069201707aa933c036af70a78df598ad530e
Bruno Viotto <brunoviottosantos@gmail.com>
2022-06-09 16:15:42 -0300

    Consertando a main

---
+++ src/app/Usuario.ts
@@ -0,0 +1,10 @@
+export interface Usuario{
+    id: Number,
+    name: string,
+    dateOfBirth: Date,
+    document: string,
+    email: string,
+    phone: string,
+    login: string,
+    passwd: string
+}
