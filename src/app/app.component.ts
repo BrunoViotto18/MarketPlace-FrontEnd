ede741e424b50ab208c93231a94e6dcb8d35dec1
Bruno Viotto <brunoviottosantos@gmail.com>
2022-06-13 14:09:15 -0300

    Coração revisado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'MarketPlace-FrontEnd';
   titlePage = "MarketPlace"
+
+  // bodyClick(event: Event){
+  //   let target = event.target as HTMLElement
+  //   let nav = document.querySelector('topbar__navigation') as HTMLElement
+  //   if (nav != target && nav.classList.contains('show-navigation'))
+  //     nav.classList.toggle('show-navigation')
+  //   document.body.style.backgroundColor = '#ff0000'
+  // }
 }
