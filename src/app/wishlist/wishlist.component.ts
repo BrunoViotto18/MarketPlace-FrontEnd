c0ae804909ea69730f363335ebb399621defb117
Bruno Viotto <brunoviottosantos@gmail.com>
2022-07-01 16:51:54 -0300

    Git

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../Stock';
+import { Component, OnInit } from '@angular/core'
+import { Product, Stock } from '../Stock'
+import axios from "axios"
 
 @Component({
   selector: 'app-wishlist',
@@ -8,12 +9,28 @@ import { Product } from '../Stock';
 })
 export class WishlistComponent implements OnInit {
 
+  stocks: Array<Stock> = []
   products: Array<Product> = []
 
   constructor() { }
 
   ngOnInit(): void {
-    
+    var config = {
+      method: 'get',
+      url: 'http://localhost:5164/WishList/getClientWishlist',
+      headers: { 
+        'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+      }
+    };
+
+    let instance = this
+    axios(config)
+    .then(function (response) {
+      instance.stocks = response.data.stocks
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
 }
